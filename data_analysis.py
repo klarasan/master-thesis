4716ca57a15df5d3771d22b17681a8bc865f5e1f
Klara <klarasan@kth.se>
2025-01-16 16:35:59 +0100

    investigated seasonality

--- data_analysis.py
+++ data_analysis.py
@@ -6,6 +6,70 @@ plt.rcParams['font.family'] = 'Times New Roman'
 
 def generate_seasonality_fig():
     # latitude ranges from -90 to 90 with 0 at equator
+    coral = (1,0.5,0.31,0.7)
+    orange = (1, 0.44, 0, 1)
+    thistle = (0.9,0.75,0.9,1)
+    plum = (0.5, 0, 0.5, 1)
+    
+
+    df = pd.read_csv('data/complete_dataset_12_years.csv', on_bad_lines='skip')
+    north = df[(df['Latitude'] >= 38) & (df['label'] == 1)]
+    mid_north = df[(df['Latitude'] >= 0) & (df['Latitude'] < 38) & (df['label'] == 1)]
+    mid_south = df[(df['Latitude'] >= -33) & (df['Latitude'] < 0) & (df['label'] == 1)]
+    south = df[(df['Latitude'] < -33) & df['label'] == 1]
+
+    n_tmax, mn_tmax, ms_tmax, s_tmax = [], [], [], []
+    n_srad, mn_srad, ms_srad, s_srad = [], [], [], []
+    n_ppt, mn_ppt, ms_ppt, s_ppt = [], [], [], []
+    for year in range(-11, -8):
+        for month in range(1, 13):      
+            n_tmax.append(north[f'tmax_year{year}_month{month}'].mean())
+            mn_tmax.append(mid_north[f'tmax_year{year}_month{month}'].mean())
+            ms_tmax.append(mid_south[f'tmax_year{year}_month{month}'].mean())
+            s_tmax.append(south[f'tmax_year{year}_month{month}'].mean())
+
+            n_srad.append(north[f'srad_year{year}_month{month}'].mean())
+            mn_srad.append(mid_north[f'srad_year{year}_month{month}'].mean())
+            ms_srad.append(mid_south[f'srad_year{year}_month{month}'].mean())
+            s_srad.append(south[f'srad_year{year}_month{month}'].mean())
+
+            n_ppt.append(north[f'ppt_year{year}_month{month}'].mean())
+            mn_ppt.append(mid_north[f'ppt_year{year}_month{month}'].mean())
+            ms_ppt.append(mid_south[f'ppt_year{year}_month{month}'].mean())
+            s_ppt.append(south[f'ppt_year{year}_month{month}'].mean())
+
+    fig, axs = plt.subplots(3, 1, figsize=(11, 9))
+    axs[0].plot(range(1,37), n_tmax, label="northern most points", color=orange)
+    axs[0].plot(range(1,37), mn_tmax, label="mid-northern most points", color=coral)
+    axs[0].plot(range(1,37), ms_tmax, label="mid-southern most points", color=thistle)
+    axs[0].plot(range(1,37), s_tmax, label="southern most points", color=plum)
+    axs[0].legend(loc='lower left')
+    axs[0].set_ylabel('Temperature (degrees Celsius)')
+    #axs[0].set_xlabel('Months')
+    #axs[0].set_title("Max temperature")
+
+    axs[1].plot(range(1,37), n_srad, label="northern most points", color=orange)
+    axs[1].plot(range(1,37), mn_srad, label="mid-northern most points", color=coral)
+    axs[1].plot(range(1,37), ms_srad, label="mid-southern most points", color=thistle)
+    axs[1].plot(range(1,37), s_srad, label="southern most points", color=plum)
+    axs[1].legend(loc='lower left')
+    axs[1].set_ylabel('Sun radiation (W/m2)')
+    #axs[1].set_xlabel('Months')
+    #axs[1].set_title("Sun exposure")
+
+    axs[2].plot(range(1,37), n_ppt, label="northern most points", color=orange)
+    axs[2].plot(range(1,37), mn_ppt, label="mid-northern most points", color=coral)
+    axs[2].plot(range(1,37), ms_ppt, label="mid-southern most points", color=thistle)
+    axs[2].plot(range(1,37), s_ppt, label="southern most points", color=plum)
+    axs[2].legend(loc='lower left')
+    axs[2].set_ylabel('Precipitation (mm)')
+    #axs[2].set_xlabel('Months')
+    #axs[2].set_title("Rain")
+
+    fig.suptitle("Seasonality over 3 years", fontsize=13)
+    #plt.tight_layout(rect=[0, 0, 1, 0.95])  # Leave space for the main title
+    plt.show()
+
     return
 
 def moving_average(data, window_size):
@@ -44,4 +108,4 @@ def generate_global_warming_fig():
     return
 
 if __name__ == "__main__":
-    generate_global_warming_fig()
+    generate_seasonality_fig()
