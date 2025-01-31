2b81982bb0a8103f17061b8985ba01e1e1993e2d
Klara <klarasan@kth.se>
2025-01-31 10:33:46 +0100

    downloaded interpolated data

--- download scripts/aggregate_data.py
+++ download scripts/aggregate_data.py
@@ -4,9 +4,8 @@ import pandas as pd
 VARS = ['aet', 'def', 'pet', 'ppt', 'q', 'soil', 'srad', 'swe', 'tmax', 'tmin', 'vap', 'ws', 'vpd', 'PDSI']
 
 def aggregate_data(num_years):
-    all_data_df = pd.read_csv('data/complete_dataset_12_years.csv', on_bad_lines='skip')
+    all_data_df = pd.read_csv('data/complete_dataset_12_years_bilinear_interp.csv', on_bad_lines='skip')
     aggr_df = all_data_df[['Ref_ID', 'Year', 'Latitude', 'Longitude']]
-    print(aggr_df)
     
     for var in VARS:
         for year in range(1-num_years, 1):
@@ -17,7 +16,7 @@ def aggregate_data(num_years):
             aggr_df[f'{var}_year{year}_std'] = temp.std(axis=1)
     
     aggr_df['label'] = all_data_df['label']
-    csv_path = f'dataset_avg_std_{num_years}_years.csv'
+    csv_path = f'dataset_avg_std_{num_years}_years_bilinear_interp.csv'
     aggr_df.to_csv(csv_path, index=False)
 
     return
